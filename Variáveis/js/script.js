108e217cfeffe33c6271bdbca585628728a62e5c
Danielle <88143024+Danielle-29@users.noreply.github.com>
2023-07-04 09:00:03 -0300

    Add files via upload

---
+++ Variáveis/js/script.js
@@ -0,0 +1,16 @@
+
+
+alert("Página carregada com sucesso!")
+
+var num1 = 5;
+var num2 = 2;
+
+var resul = (num1 * num2);
+var resul1 = (num1 + num2);
+var resul2 = (num1 - num2);
+var resul3 = (num1 / num2);
+
+console.log(resul)
+console.log(resul1)
+console.log(resul2)
+console.log(resul3)
